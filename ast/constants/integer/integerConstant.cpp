efd54b49d57a0e9ca1419a14d06cde5c4219c9a8
PatriotRossii <patriotrossii2019@mail.ru>
2021-12-30 14:54:03 +0500

    Ceil bitWidth to smallest that fits

--- ast/constants/integer/integerConstant.cpp
+++ ast/constants/integer/integerConstant.cpp
@@ -4,6 +4,7 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/Type.h"
 
 #include "integerConstant.hpp"
 #include "integerConstantType.hpp"
@@ -17,17 +18,22 @@ IntegerConstant::IntegerConstant(llvm::APInt value): value(value) { }
 llvm::Value* IntegerConstant::codegen() {
 	auto codegenState = fgs::codegen::CodegenState{};
 	return llvm::ConstantInt::get(
-		llvm::Type::getInt64Ty(codegenState.context),
-		value
+		codegenState.context, value
 	);
 }
 
 IntegerConstant IntegerConstant::parse(const std::string& number) {
 	auto type = IntegerConstantType(number);
 	std::string extractedInteger = extractInteger(number, type);
-	return IntegerConstant(
-		llvm::APInt(64, extractedInteger, static_cast<uint8_t>(type.getFormatType()))
-	);
+	llvm::APInt value = llvm::APInt(64, extractedInteger, static_cast<uint8_t>(type.getFormatType()));
+
+	unsigned bitWidth = value.ceilLogBase2();
+
+	if(bitWidth <= 16) bitWidth = 16;
+	else if(bitWidth <= 32) bitWidth = 32;
+	value = value.trunc(bitWidth);
+
+	return IntegerConstant(value);
 }
 
 }
